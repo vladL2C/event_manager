d5bde1d7de40d5e5a751c8f60c4b295a64786390
Vlad <vlad@hotmail.co.nz>
2017-06-25 16:45:35 -0700

    finished the eventmanage jumpstart tutorial

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,32 +1,45 @@
 require 'csv'
 require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 def clean_zipcode(zipcode)
-	zipcode.to_s.rjust(5,"0")[0..4]
+  zipcode.to_s.rjust(5,"0")[0..4]
 end
 
 def legislators_by_zipcode(zipcode)
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+  Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
 
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
-  end
-  legislator_names.join(", ")
-end   
+def save_thank_you_letters(id,form_letter)
+	Dir.mkdir("output") unless Dir.exists?("output")
 
-puts "EventManager initialized"
+	filename = "output/thanks_#{id}.html"
 
-contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+	file.open(filename,'w') do |file|
+		file.puts form_letter
+	end 
+
+puts "EventManager initialized."
+
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
 
 contents.each do |row|
-  name = row[:first_name]
+	id = row[0]
+	name = row[:first_name]
 
-  zipcode = clean_zipcode(row[:zipcode])
+	zipcode = clean_zipcode(row[:zipcode])
 
-  legislators = legislators_by_zipcode(zipcode)
+	legislators = legislators_by_zipcode(zipcode)
 
-  	puts "#{name} #{zipcode} #{legislators}"
-end
+	form_letter = erb_template.result(binding)
+
+	save_thank_you_letters(id,form_letter)
+
+end 
+
 
